c01d646a7316e42ab46804cd2d466837e1fba8f6
moustafa ezzat <118759254+Moustafa24@users.noreply.github.com>
2024-06-21 19:11:51 +0300

    Add files via upload

---
+++ moustafa ezzat/db.sql
@@ -0,0 +1,13 @@
+/*
+
+-- DATABASE name "student" --
+
+*/
+
+CREATE TABLE `nstudent ` (
+  `id` int (11) NOT NULL,
+ `name`   varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gpa` varchar(255) NOT NULL,
+  `selecting` ENUM 
+) 
