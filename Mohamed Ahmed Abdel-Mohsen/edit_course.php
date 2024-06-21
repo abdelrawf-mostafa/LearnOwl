80e0dedca4edaa0fdeaab9577d31701605abff37
Mohamed-Ahmed007062 <163561986+Mohamed-Ahmed007062@users.noreply.github.com>
2024-06-21 02:20:32 +0200

    Create edit_course.php

---
+++ Mohamed Ahmed Abdel-Mohsen/edit_course.php
@@ -0,0 +1,50 @@
+<?php
+include "connect.php";
+if (isset($_POST['title']) && isset($_POST['description']) && isset($_POST['instructor']))  {
+    function validata($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $title = validata($_POST['title']);
+    $description = validata($_POST['description']);
+    $instructor = validata($_POST['instructor']);
+
+    if (empty($title)) {
+        header("Location: index.php?error=Title is required");
+    exit();
+    }
+    else if (empty($description)) {
+        header("Location: index.php?error=Description is required");
+        exit();
+    }
+    else if (empty($instructor)) {
+        header("Location: index.php?error=Instructor is required");
+        exit();
+    }
+    else {
+        $id = 1;
+        $sql = "UPDATE courses SET title='$title', description='$description', instructor='$instructor' WHERE id='$id'";
+
+        if (mysqli_query($conn, $sql)) {
+            if (mysqli_affected_rows($conn) > 0) {
+                header("Location: index.php?error= No Course updated ");
+            } else {
+                header("Location: index.php?success=Course updated successfully");
+            }
+        } else {
+            header("Location: index.php?error=Error updating course: " . mysqli_error($conn));
+        }
+        exit();
+    }
+
+
+}
+else {
+    header("Location: index.php");
+    exit();
+}
+?>
