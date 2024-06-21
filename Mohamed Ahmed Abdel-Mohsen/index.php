c727657c06a36d705f0d077e1b43bc432222c897
Mohamed-Ahmed007062 <163561986+Mohamed-Ahmed007062@users.noreply.github.com>
2024-06-21 02:18:39 +0200

    Create index.php

---
+++ Mohamed Ahmed Abdel-Mohsen/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Course</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Edit Course</h1>
+        <br>
+        <br>
+        <form method="post" action="edit_course.php">
+            <?php if (isset($_GET['error'])) { ?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+                <?php }?>
+            <label>Title:</label>
+            <input type="text" name="title" value="" placeholder="Text"><br>
+            <label>Description:</label>
+            <textarea name="description" placeholder="Description"></textarea><br>
+            <label>Instructor:</label>
+            <input type="text" name="instructor" value="" placeholder="Instructor"><br>
+            <input type="submit" value="Update Course">
+        </form>
+        <br>
+        <a href="courses.php">Back to Dashboard</a>
+    </div>
+</body>
+</html>
